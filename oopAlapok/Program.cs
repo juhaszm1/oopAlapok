6e52d854021a947b4feed70c10ce8d7debc67355
Juhász Márk <juhaszm@KKSZKI.LOCAL>
2025-09-24 11:25:38 +0200

    _feladat2ready

---
+++ oopAlapok/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopAlapok
+{
+    public class Szemely
+    {
+        public string _nev = "Nagy Pista";
+        public int _kor = 40;
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Szemely tanulo1 = new Szemely();
+            Console.WriteLine(tanulo1._nev);
+            Console.WriteLine(tanulo1._kor);
+        }
+    }
+}
