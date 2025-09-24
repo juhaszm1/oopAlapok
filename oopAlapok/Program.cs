f6951c970eb8e62665be02416475676ebc6844c8
Juhász Márk <juhaszm@KKSZKI.LOCAL>
2025-09-24 12:07:00 +0200

    Kesza3

--- oopAlapok/Program.cs
+++ oopAlapok/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,47 @@ namespace oopAlapok
         public string _nev = "Nagy Pista";
         public int _kor = 40;
 
+        public Szemely()
+        {
+        }
+
+        public Szemely(string nev)
+        {
+            _nev = nev;
+            
+        }
+
+        public string Nev
+        {
+            get { return _nev; }
+            //set { _nev = value; }
+                
+            
+        }
+        
+
+        public int Kor
+        {
+            get { return _kor; }
+            set
+            {
+                if (value >= 0) 
+                    _kor = value;
+                else
+                    Console.WriteLine("Hibas kor");
+                
+            }
+        }
+
+
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Szemely tanulo1 = new Szemely();
-            Console.WriteLine(tanulo1._nev);
-            Console.WriteLine(tanulo1._kor);
+            Szemely tanulo1 = new Szemely("Nagy Pista");
+            tanulo1.Kor = 44;
+            Console.WriteLine(tanulo1.Nev + tanulo1.Kor);
         }
     }
 }
